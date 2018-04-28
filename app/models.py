44080e9fc88ce8dba3e3aee34ec1354616f19bd3
VickyMutai <vicky.mutai96@gmail.com>
2018-04-28 16:32:10 +0300

    adding api based views

--- app/models.py
+++ app/models.py
@@ -1,3 +1,42 @@
+from uuid import uuid4
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+def deserialize_user(user):
+    """Deserialize user instance to JSON."""
+    return {
+        'id': user.id, 'username': user.username, 'email': user.email,
+    }
+
+class TrackDate(models.Model):
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+def _generate_unique_uri():
+    """Generates a unique uri for the chat session."""
+    return str(uuid4()).replace('-', '')[:15]
+
+class Chat(TrackDate):
+    admin = models.ForeignKey(User, on_delete=models.PROTECT)
+    uri = models.URLField(default=_generate_unique_uri)
+
+class ChatMessage(TrackDate):
+    """Store messages for a session."""
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    chat = models.ForeignKey(Chat, related_name='messages', on_delete=models.PROTECT)
+    message = models.TextField(max_length=2000)
+
+    def convert(self):
+        return{'user':deserialize_user(self.user),
+                'message':self.message}
+
+class ChatMember(TrackDate):
+    chat = models.ForeignKey(Chat, related_name='members', on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
--- app/views.py
+++ app/views.py
@@ -1,3 +1,112 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from .models import Chat,ChatMember,ChatMessage,deserialize_user
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
 
 # Create your views here.
+class ChatView(APIView):
+    permission_classes = (permissions.IsAuthenticated)
+    def post(self,request,*args,**kwargs):
+        user = request.user
+        chat = Chat.objects.create(admin=user)
+
+        context = {
+            'status':'SUCCESS',
+            'uri':chat.uri,
+            'message':'New room created'
+        }
+        return Response(context)
+
+    def add(self,request,*args,**kwargs):
+        User = get_user_model()
+        uri = kwargs['uri']
+        username = request.data['username']
+        user = User.objects.get(username=username)
+
+        chat=Chat.objects.get(uri=uri)
+        admin = chat.admin
+
+        if admin != user:
+            chat.members.get_or_create(user=user,chat=chat)
+
+        admin = deserialize_user(admin)
+        members = [ deserialize_user(chat.user) for chat in chat.members.all()]
+        members.insert(0,admin)
+
+        context = {
+            'status':'SUCCESS',
+            'members':members,
+            'message':'%s joined that chat' %user.username,
+            'user':deserialize_user(user)
+        }
+
+        return Response(context)
+
+class ChatMessageView(APIView):
+    permission_classes = (permissions.IsAuthenticated)
+    def get(self,request,*args,**kwargs):
+        uri = kwargs['uri']
+        chat = Chat.objects.get(uri=uri)
+        messages [chat_message.to_json() for chat_message in chat.messages.all()]
+
+        context = {
+            'id':chat.id,
+            'uri':chat.uri,
+            'messages':messages,
+        }
+        return Response(context)
+
+    def post(self,request,*args,**kwargs):
+        uri = kwargs['uri']
+        message = request.data['message']
+        user = request.user
+        chat= Chat.objects.get(uri=uri)
+        ChatMessage.objects.create(user=user, chat=chat, message=message)
+
+        context={
+            'status': 'SUCCESS',
+            'uri': chat_session.uri, 
+            'message': message,
+            'user': deserialize_user(user)
+        }
+        return Response (context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
